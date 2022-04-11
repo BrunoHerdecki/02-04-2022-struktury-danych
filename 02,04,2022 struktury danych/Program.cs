20c5a497d99e535a6d6c9f744bc35a019864cc78
BrunoHerdecki <bruno.herdecki@microsoft.wsei.edu.pl>
2022-04-11 22:51:10 +0200

    das

--- 02,04,2022 struktury danych/Program.cs
+++ 02,04,2022 struktury danych/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02_04_2022_struktury_danych
 {
@@ -6,20 +7,141 @@ namespace _02_04_2022_struktury_danych
     {
         static void Main(string[] args)
         {
-            Node<int> head =
-            new Node<int>(2,
-            new Node<int>(5,
-            new Node<int>(1)));
-            AddAtEndOfSingleLinkedList<int>(-1, head);
+
+            var head = CreateSingleLinkedList<int>(2, 1, 2, 2, 1, 5, 1);
             PrintSingleLinkedList<int>(head);
+            RemoveAllDuplicatesFromSortedLinkedList<int>(ref head);
+            PrintSingleLinkedList<int>(head);
+
         }
 
-        public static void AddAtEndOfSingleLinkedList<T>(T element, Node<T> head)
+        public static void RemoveAllDuplicatesFromSortedLinkedList<T>(ref Node<T> head)
+        where T : IEquatable<T>, IComparable<T>
         {
-            
-           
+            Node<T> current = head;
+            while (current != null)
+            {
+                Node<T> prev = current;
+                int position = 1;
+                while (prev != null)
+                {
+                    Console.WriteLine(); PrintSingleLinkedList<T>(head);
+                    if (prev.Next != null && EqualityComparer<T>.Default.Equals(current.Data, prev.Next.Data))
+                    {
+                        RemoveNodeAt<T>(position, ref head);
+                    }
+                    prev = prev.Next;
+                    position++;
+                }
+                current = current.Next;
+            }
 
+        }
 
+        public static void RemoveNodeAt<T>(int position, ref Node<T> head)
+        {
+            Node<T> current = head;
+            int l = 0;
+            int c = 0;
+
+            while (current != null)
+            {
+                current = current.Next;
+                c++;
+            }
+            current = head;
+            if (position == c)
+            {
+                return;
+            }
+
+            if (current == null || current.Next == null)
+            {
+                return;
+            }
+            if (position == 0)
+            {
+                current = current.Next;
+                head = current;
+
+            }
+            else
+            {
+                while (l < position - 1)
+                {
+                    current = current.Next;
+                    l++;
+                }
+                current.Next = current.Next.Next;
+            }
+
+        }
+
+        public static Node<T> CreateSingleLinkedList<T>(params T[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                return null;
+            }
+
+            Node<T> head = new Node<T>(arr[0]);
+            Node<T> x = head;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                x.Next = new Node<T>(arr[i]);
+                x = x.Next;
+            }
+            return head;
+        }
+        public static void AddAtEndOfSingleLinkedList<T>(T element, ref Node<T> head)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(element);
+            }
+            else
+            {
+                Node<T> k = head;
+                while (k.Next != null)
+                {
+                    k = k.Next;
+                }
+                k.Next = new Node<T>(element);
+            }
+        }
+        public static Node<T> ReverseSingleLinkedList<T>(Node<T> head)
+        {
+            Node<T> prev = null, current = head, next = null;
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+            return head;
+        }
+
+        public static void MoveLastNodeToFront<T>(ref Node<T> head)
+        {
+            var current = head;
+            Node<T> prev = head;
+            if (current == null || current.Next == null)
+            {
+                return;
+            }
+            while (current.Next != null)
+            {
+                if (current.Next.Next == null)
+                {
+                    prev = current;
+                }
+                current = current.Next;
+            }
+            prev.Next = null;
+            current.Next = (head);
+            head = current;
         }
 
         public static void PrintSingleLinkedList<T>(Node<T> head)
@@ -34,7 +156,7 @@ namespace _02_04_2022_struktury_danych
                 Console.Write("head -> ");
                 while (p != null)
                 {
-                    Console.Write(p.Data + " -> ");
+                    Console.Write(p.ToString());
                     p = p.Next;
                 }
                 Console.Write("null");
